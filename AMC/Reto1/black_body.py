339e4257e76e56ee9867591793e68e6e225476a6
Giovanni <giovanniloez9808@gmail.com>
2020-09-08 15:43:45 -0500

    [ADD] Wien law

--- AMC/Reto1/black_body.py
+++ AMC/Reto1/black_body.py
@@ -44,32 +44,3 @@ r2=r2_score(spec,fit)
 print("Temperatura de fit",pars[0])
 print("La diferencia relativa es",round(np.mean(var)*100,4))
 print("El coeficiente de determinacion es",r2)
-#<------------------------------Calculo para diferentes temperaturas----------------------------->
-lon=np.arange(0.2,3.2,0.01)*1e-6
-#<--------------------------Longitudes a frecuencias----------------------------------------->
-freq=c/lon
-#<---------------------------Lista de temperaturas----------------------------->
-T_list=[3000,3500,4000,4500,5000,5500,6000]
-T_color=["F48C06","e85d04","DC2F02","9D0208","6A040F","370617","03071E"]
-max_lon=[];max_spec=[]
-for T,color in zip(T_list,T_color):
-    spec=black_body_freq(freq,T)
-    #<------------------------------W/m2srHZ -> PBjy/sr----------------------------------------->
-    spec=spec*10**(26-15)
-    #<--------------------------Localizacion del especto maximo--------------------------------->
-    max_point=np.max(spec)
-    max_spec=np.append(max_spec,max_point)
-    #<--------------------------------Localizacion de la localizacion---------------------------------->
-    loc=np.where(max_point==spec)[0]
-    max_lon=np.append(max_lon,lon[loc]*1e9)
-    #<------------------------------------Grafica de cada temperatura------------------------------->
-    plt.plot(lon*1e9,spec,label=str(T)+" K",lw="4",color="#"+color)
-plt.plot(max_lon,max_spec,label="Ley de Wien",color="#2b2d42",ls="--")
-plt.ylabel("Intensity (PBJy/sr)")
-plt.xlabel("Wavelength (nm)")
-plt.xlim(200,3200)
-plt.ylim(0,5000)
-plt.xticks(np.arange(200,3700,500))
-plt.yticks(np.arange(0,5500,500))
-plt.legend(frameon=False,mode="expand",ncol=4)
-plt.savefig("Graphics/black_body.png",dpi=100)
