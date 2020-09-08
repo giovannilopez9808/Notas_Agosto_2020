ececfb869cce635685dba98e5fa6bcedcf523eee
Giovanni <giovanniloez9808@gmail.com>
2020-09-07 21:08:06 -0500

    [ADD] Tarea1 MCR [CHANGE] fit.py

--- AMC/Reto1/fit.py
+++ AMC/Reto1/fit.py
@@ -2,29 +2,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from scipy import constants as C
+from sklearn.metrics import r2_score
 #<------------------------Funcion de la radiacion de cuerpo negro------------------>
 def black_body_freq(v,T):
     a=2*h*v**3/c**2
     b=h*v/(k*T)
     i=a/(np.exp(b)-1)
     return i
-#<-----------------------Constante de Boltzmann------------------------------------>
+#<-----------------------Constantes--------------------------------------------->
 c=C.c;k=C.Boltzmann;h=C.h;pi=np.pi
 #<----------------------Lectura de los datos----------------------------------->
 lon,spec=np.loadtxt("data.txt",unpack=True,skiprows=18,usecols=[0,1])
-#<-------------Convertir unidades de medicion------------------------->
+#<-------------Convertir unidades de medicion------------------------>
+#<----------------------cm^-1 -> Hz------------------------------------>
 freq=(lon*100)*c
-spec2=spec*1e-20
+#<---------------------Mjy/sr -> W/m2srHz----------------------------------------->
+spec=spec*1e-20
 #<-----------------------Fit------------------------------------------->
-pars, cov = curve_fit(f=black_body_freq,xdata=freq,ydata=spec2,p0=[5])
+pars,cov=curve_fit(f=black_body_freq,xdata=freq,ydata=spec,p0=[5])
 fit=black_body_freq(freq,pars[0])
-print(cov)
+#<------------------------------W/m2srHZ -> Mjy/sr----------------------------------------->
+spec=spec*1e20
+fit=fit*1e20
+#<---------------------------Hz -> Gz--------------------------------------->
+freq=freq*1e-9
 #<---------------------------------Inicio de la grafica----------------------------->
 plt.xlabel("Frequency (GHz)")
 plt.ylabel("Intensity (MJy/sr)")
-plt.ylim(0,450)
-plt.xlim(0,700)
-plt.scatter(freq*1e-9,spec2*1e20,marker="o",label="COBE data")
-plt.plot(freq*1e-9,fit*1e20,label="Fit",color="green")
+plt.ylim(0,425)
+plt.xlim(0,650)
+plt.scatter(freq,spec,marker="o",label="COBE data")
+plt.plot(freq,fit,label="Fit",color="green")
 plt.legend(ncol=2,mode="expand",frameon=False)
+plt.grid(ls="--",color="grey")
 plt.savefig("Graphics/fit.png")
+#<---------------------------Calculo de la diferencia relativa--------------------->
+var=(np.abs(spec-fit))/spec
+#<-----------------------------Calculo de la R2---------------------------------->
+r2=r2_score(spec,fit)
+print("La diferencia relativa es",round(np.mean(var)*100,4))
+print("El coeficiente de determinacion es",r2)
