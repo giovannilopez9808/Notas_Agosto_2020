9374f1b7bb243602f617482b6f3e2d68a583c119
Giovanni <giovanniloez9808@gmail.com>
2020-09-20 23:50:10 -0500

    [ADD] Change in graphic

--- Simulaciones/Proyecto_1/Scripts/Cor_Graphics.py
+++ Simulaciones/Proyecto_1/Scripts/Cor_Graphics.py
@@ -20,6 +20,9 @@ for rho in rho_values:
     plt.clf()
 fig,((ax1,ax2,ax3),(ax4,ax5,ax6),(ax7,ax8,ax9))=plt.subplots(3,3,figsize=(9,7))
 axs=[ax1,ax2,ax3,ax4,ax5,ax6,ax7,ax8,ax9]
+
+x_lim=[0.9,4.9];y_lim=[0,5];delta=[0.6,1]
+xtick=np.arange(x_lim[0],x_lim[1]+delta[0],delta[1]);ytick=np.arange(y_lim[0],y_lim[1]+delta[1],delta[1])
 for rho,ax in zip(rho_values,axs):
     #<------------------------------------Grafica de la distribucion radial------------------------->
     r2,hist2=np.loadtxt(dir_results2+"4_hr_"+str(rho)+".dat",unpack=True)
@@ -27,18 +30,21 @@ for rho,ax in zip(rho_values,axs):
     ax.plot(r2,hist2,label="R$^{2}$",color="#6a040f")
     ax.set_title("$\\rho=$"+str(rho))
     ax.plot(r3,hist3,label="R$^{3}$",color="#0096c7")
-    ax.set_xlim(0.9,4)
-    ax.set_ylim(0,5)
+    ax.set_xlim(x_lim[0],x_lim[1])
+    ax.set_ylim(y_lim[0],y_lim[1])
     if ax in [ax1,ax4,ax7]:
-        ax.set_yticks(np.arange(0,5+1,1))
+        ax.set_yticks(ytick)
         ax.set_ylabel("$ \\rho (r)$")
     else:
         ax.set_yticks([])
     if ax in [ax7,ax8,ax9]:
-        ax.set_xticks(np.arange(0.9,5+0.6,0.6))
+        ax.set_xticks(xtick)
         #ax.set_xlabel("Distancia radial (r)")
     else:
         ax.set_xticks([])
-    ax.legend(frameon=False,ncol=2)
+    ax.legend(frameon=False,ncol=2,bbox_to_anchor=(0,1,1,0),mode="expand")
+    for x,y in zip(xtick,ytick):
+        ax.plot([x,x],[ytick[0],ytick[-1]],ls="--",color="black",alpha=0.5)
+        ax.plot([xtick[0],xtick[-1]],[y,y],ls="--",color="black",alpha=0.5)
 ax8.set_xlabel('Distancia radial(r)')
 plt.savefig(dir_graphics+"Dis_rad.png",dpi=300)
