70d2867e55e269333b82d4de8b43e049b2d97ad4
Giovanni <giovanniloez9808@gmail.com>
2020-10-01 13:01:19 -0500

    [ADD] New graphics

--- Simulaciones/Proyecto_2/Scripts/Temp_Graphics.py
+++ Simulaciones/Proyecto_2/Scripts/Temp_Graphics.py
@@ -8,18 +8,18 @@ temp_values=np.round(np.arange(0.3,1.3,0.2),2)
 fig,axs_total=plt.subplots(5,3,figsize=(12,7))
 axs_total=np.transpose(axs_total)
 colors=["#03071e","#6a040f","#9d0208","#e85d04","#faa307"]
+lim_list=[[0.25,0.4,0.05],[0.4,0.6,0.05],[0.4,0.8,0.1],[0.6,1,0.1],[0.8,1.4,0.2]]
 for rho,axs in zip(rho_values,axs_total):
-    for t,ax,color in zip(temp_values,axs,colors):
+    for t,ax,color,lim in zip(temp_values,axs,colors,lim_list):
         #<------------------Lectura de los datos------------------------->
-        version="8_T_U_P_"+str(rho)+"_"+str(t)+".dat"
-        walks,e_kin,e_pot=np.loadtxt(dir+version,unpack=True,usecols=[0,1,2])
-        e_tol=e_pot*1e3+e_kin
+        version="temp_"+str(rho)+"_"+str(t)+".dat"
+        walks,temp=np.loadtxt(dir+version,unpack=True)
         walks=walks*1e-4
-        ax.plot(walks,e_tol,color=color)
-        min=np.min(e_tol)
-        max=np.max(e_tol)
-        yticks=np.round(np.linspace(min,max,5),2)
-        ax.set_yticks(yticks)
+        ax.plot(walks,temp,color=color,lw=3)
+        ax.set_ylim(lim[0],lim[1])
+        ax.set_yticks(np.round(np.arange(lim[0],lim[1]+lim[2],lim[2]),2))
+        ax.set_xlim(walks[0],walks[-1])
+        ax.plot([walks[0],walks[-1]],[t,t],ls="--",color="green")
         #<-----------------Añadir el titulo centrado---------------------->
         if ax==axs[0]:
              ax.set_title("$\\rho=$"+str(rho))
@@ -32,5 +32,5 @@ for rho,axs in zip(rho_values,axs_total):
             ax.set_ylabel("$T=$"+str(t))
         #<-----------------------------------Guardado de la gráfica------------------------->
 fig.text(0.5, 0.04, 'Walks (10$^{3}$)', ha='center',fontsize=14)
-fig.text(0.04, 0.5, 'Energía total', va='center', rotation='vertical',fontsize=14)
-plt.savefig(dir_graphics+"Energy.png",dpi=200)
+fig.text(0.04, 0.5, 'Temperatura', va='center', rotation='vertical',fontsize=14)
+plt.savefig(dir_graphics+"Temp.png",dpi=200)
