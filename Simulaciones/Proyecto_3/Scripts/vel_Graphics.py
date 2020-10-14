eee34a67a602f5c6ca86ed74f111bdc6fd3af524
Giovanni <giovanniloez9808@gmail.com>
2020-10-13 21:49:19 -0500

    [ADD] Falta conclusion

--- Simulaciones/Proyecto_3/Scripts/vel_Graphics.py
+++ Simulaciones/Proyecto_3/Scripts/vel_Graphics.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 #<----------------------------Direcciones de los archivos-------------------->
 dir="../Results/";dir_graphics="../Graphics/"
 rho_values=np.round(np.arange(0.4,0.8,0.1),2)
@@ -8,7 +9,7 @@ n_pasos=20
 n_part=784
 fig,axs=plt.subplots(2,2,figsize=(8,6))
 axs=np.reshape(axs,4)
-x_lim,y_lim=[-4,4,1],[0,3000,500]
+x_lim,y_lim=[-4,4,1],[0,3500,500]
 xtick=np.arange(x_lim[0],x_lim[1]+x_lim[2],x_lim[2])
 ytick=np.arange(y_lim[0],y_lim[1]+y_lim[2],y_lim[2])
 for rho,ax,color in zip(rho_values,axs,colors):
@@ -17,8 +18,12 @@ for rho,ax,color in zip(rho_values,axs,colors):
         data_x,data_y=np.loadtxt(dir+"2_velo_"+str(rho)+".dat",unpack=True,skiprows=i*(n_part+1)+1,max_rows=n_part)
         vel=np.append(vel,data_x)
         vel=np.append(vel,data_y)
-    ax.hist(vel,bins=50,color=color)
+    ax.hist(vel,bins=25,color=color,label="Data")
     ax.set_ylim(y_lim[0],y_lim[1])
+    mu, std = norm.fit(vel)
+    x = np.linspace(x_lim[0],x_lim[1], 100)
+    p = norm.pdf(x, mu, std)*10**(4)
+    ax.plot(x,p,lw=3,color="#f48c06",label="Fit")
     #<---------------------Ticks en y--------------------------------------->
     ax.set_yticks(ytick)
     #<-----------------Añadir el titulo centrado---------------------->
