78bc9a48de907a498b0b0ca0be822f2273d56392
Giovanni <giovannilopez9808@gmail.com>
2020-10-23 01:34:53 -0500

    [ADD] Terminado antes de revisiones

--- AMC/Proyecto_final/Scripts/General/Time_graphics.py
+++ AMC/Proyecto_final/Scripts/General/Time_graphics.py
@@ -5,9 +5,12 @@ clasic=clasic*1e3
 quantum=np.loadtxt("time_shor.txt")
 quantum=quantum/4
 quantum_ibm=quantum/1.5
+print("Clasico",round(np.mean(clasic),3),round(np.std(clasic),4))
+print("Quantum",round(np.mean(quantum),3),round(np.std(quantum),4))
+print("Quantum IBM",round(np.mean(quantum_ibm),3),round(np.std(quantum_ibm),4))
 x=np.arange(np.size(clasic))
 plt.xlim(0,1000)
-plt.scatter(x,clasic,marker=".",color="#03071e",label="Clásico")
+plt.scatter(x,clasic,marker=".",color="#03071e",label="Clásico CCGN")
 plt.scatter(x,quantum,marker=".",color="#9d0208",label="Shor Local")
 plt.scatter(x,quantum_ibm,marker=".",color="#f48c06",label="Shor IBM")
 plt.legend(frameon=False,ncol=3,mode="expand",bbox_to_anchor=(0, 1.06,1,0.02),markerscale=2, scatterpoints=1,)
@@ -17,4 +20,4 @@ plt.xticks(np.arange(0,1000+100,100))
 plt.xlabel("Número de repeticiones")
 plt.ylabel("Tiempo (ms)")
 plt.subplots_adjust(left=0.121,bottom=0.112,right=0.943,top=0.898)
-plt.savefig("time.png",dpi=200
+plt.savefig("time.png",dpi=200)
