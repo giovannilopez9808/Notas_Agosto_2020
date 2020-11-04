1916de84c43eac5353b0af645db27ae1a5194c89
gio <giovannilopez9808@gmail.com>
2020-11-04 13:16:40 -0600

    [ADD] Tarea5

--- AMC/Tarea5/eigen.py
+++ AMC/Tarea5/eigen.py
@@ -2,4 +2,6 @@ import numpy as np
 a=1/np.sqrt(2)
 H=[[a,a],[a,-a]]
 eval,evec=np.linalg.eig(H)
-print(eval,evec)
+evec=np.transpose(evec/evec[1,1])
+print("Los eigenvalores son \n",eval)
+print("Las eigenvectores son \n",evec)
